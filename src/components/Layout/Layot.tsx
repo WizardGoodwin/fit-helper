bddbf8ea5d5cf3dbe9ed31b95a3ed1ec113aad89
WizardGoodwin <patrushev.maxim@gmail.com>
2019-09-30 13:52:50 +0300

    markup fix

--- src/components/Layout/Layot.tsx
+++ src/components/Layout/Layot.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, FunctionComponent } from 'react';
 
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 interface IProps {
   children: any;
@@ -11,10 +10,9 @@ const Layout: FunctionComponent<IProps> = (props) => {
   return (
     <Fragment>
       <Header />
-      <main className="container row mx-auto mb-5">
-        <div className="col-lg-9">{props.children}</div>
+      <main>
+        {props.children}
       </main>
-      <Footer />
     </Fragment>
   );
 };
